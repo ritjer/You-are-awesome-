1e584ef3dc771a12390df02271248765092c118b
Richard Stenstrom <richard_stenstrom@hotmail.com>
2022-10-03 15:27:30 -0400

    Initial Commit

---
+++ You are awesome!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You are awesome!
+//
+//  Created by Richard Stenstrom on 2022-10-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
